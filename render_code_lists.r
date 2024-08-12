7b8845de81a32273d974144584ebc369a6daf011
Tony Stone <tony-stone@users.noreply.github.com>
2024-08-12 16:49:06 +0100

    Revert " Make download links download (rather than open) files (#74)"
    This reverts commit 63e44836363820f5b42b6645021397c58293eac3.

--- render_code_lists.r
+++ render_code_lists.r
@@ -18,8 +18,6 @@ write("\n", quarto_fileConn, append = T)
 write(paste0("    - part: ", index_filename), quarto_fileConn, append = T)
 write("      chapters:", quarto_fileConn, append = T)
 
-phenotype_repo_raw_content_prefix <- "https://raw.githubusercontent.com/UCL-ECHILD/phenotype-code-lists/main/"
-
 for (i in 1:nrow(code_lists)) {
   
   ## Create one .qmd file per code list
@@ -78,22 +76,22 @@ for (i in 1:nrow(code_lists)) {
   write(paste0("| **id** | ", id, " |"), fileConn, append = T)
   write(paste0("| **Previous version(s)** | ", prev_vers, " |"), fileConn, append = T)
   write(paste0("| **Number of files** | ", n_files, " |"), fileConn, append = T)
-  write(paste0("| **Code list file** | <a href=\"", phenotype_repo_raw_content_prefix, codelist_file, "\" download>", id, "</a> |"), fileConn, append = T)
+  write(paste0("| **Code list file (right-click to download)** | [", id, ".csv](", codelist_file, "){target=\"_blank\"} |"), fileConn, append = T)
   if (!is.na(restrictions_file)) {
     restrictions_filename <- paste0(id, "_restrictions.csv")
-    write(paste0("| **Restrictions file ** | <a href=\"", phenotype_repo_raw_content_prefix, restrictions_file, "\" download>", restrictions_filename, "</a> |"), fileConn, append = T)
+    write(paste0("| **Restrictions file ** | [", restrictions_filename, "](", restrictions_file, "){target=\"_blank\"} |"), fileConn, append = T)
   } else {
     write(paste0("| **Restrictions file ** | NA |"), fileConn, append = T)
   }
   if (!is.na(truncations_file)) {
     truncations_filename <- paste0(id, "_trunactions.csv")
-    write(paste0("| **Truncations file ** | <a href=\"", phenotype_repo_raw_content_prefix, truncations_file, "\" download>", truncations_filename, "</a> |"), fileConn, append = T)
+    write(paste0("| **Truncations file ** | [", truncations_filename, "](", truncations_file, "){target=\"_blank\"} |"), fileConn, append = T)
   } else {
     write(paste0("| **Truncations file ** | NA |"), fileConn, append = T)
   }
   if (!is.na(removals_file)) {
     removals_filename <- paste0(id, "_removals.csv")
-    write(paste0("| **Removals file ** | <a href=\"", phenotype_repo_raw_content_prefix, removals_file, "\" download>", removals_filename, "</a> |"), fileConn, append = T)
+    write(paste0("| **Removals file ** | [", removals_filename, "](", removals_file, "){target=\"_blank\"} |"), fileConn, append = T)
   } else {
     write(paste0("| **Removals file ** | NA |"), fileConn, append = T)
   }
